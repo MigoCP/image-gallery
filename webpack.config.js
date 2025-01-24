80dc8caa6c16d2a4def71329185f56a8bd3be5df
QueenSarah21 <128749996+Queen-Sarah21@users.noreply.github.com>
2025-01-23 21:41:07 -0500

    Re-arranged the structure of the files. Added main.css and main.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,46 @@
-const path = require('path');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/gallery.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'], // Compile SCSS to CSS
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'], // Transpile modern JS
-                    },
-                },
-            },
-        ],
-    },
-    mode: 'development', // Switch to 'production' for optimized builds
+  entry: {
+    main: "./scripts/main.js",
+    gallery: "./scripts/gallery.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      chunks: ["main"],
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./gallery.html",
+      chunks: ["gallery"],
+      filename: "gallery.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 9000,
+  },
 };
